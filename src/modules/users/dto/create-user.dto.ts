ea17053c7308701dbeeb9ff05a28513b9f76911e
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-06 12:25:59 +0300

    adding create user dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,42 @@
-export class CreateUserDto {}
+import { Transform } from "class-transformer";
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    IsUUID,
+    Length,
+} from "class-validator";
+
+export class CreateUserDto {
+    // @IsUUID()
+    // @Transform(({ value }) => value.toString())
+    // role_id: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(8, 45)
+    user_name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(3, 45)
+    first_name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(3, 45)
+    last_name: string;
+
+    @IsOptional()
+    pic?: string;
+
+    @IsEmail()
+    @Length(1, 255)
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(8, 50)
+    password: string;
+}
