54318d4512b21ab12f2536dd93dd0e50e4a0d946
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-06 20:35:42 +0300

    adding the create role dto

--- src/modules/roles/dto/create-role.dto.ts
+++ src/modules/roles/dto/create-role.dto.ts
@@ -1,1 +1,14 @@
-export class CreateRoleDto {}
+import { Transform } from "class-transformer";
+import { IsOptional, IsString, Length } from "class-validator";
+
+export class CreateRoleDto {
+    @Transform(({ value }) => String(value).trim().toLocaleLowerCase())
+    @IsString()
+    @Length(3, 45)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(3, 45)
+    description: string;
+}
