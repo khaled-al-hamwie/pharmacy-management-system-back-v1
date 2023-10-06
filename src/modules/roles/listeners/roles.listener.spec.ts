930556cf83d97dd1c7a7ceceedfbf0c5146ea26d
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-06 20:43:56 +0300

    adding role listener save test

--- src/modules/roles/listeners/roles.listener.spec.ts
+++ src/modules/roles/listeners/roles.listener.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Role } from "../entities/role.entity";
 import { RolesListener } from "./roles.listener";
+
+const mockRepository = {
+    save: jest.fn(),
+};
 
 describe("RolesListener", () => {
     let provider: RolesListener;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [RolesListener],
+            providers: [
+                RolesListener,
+                { provide: getRepositoryToken(Role), useValue: mockRepository },
+            ],
         }).compile();
 
         provider = module.get<RolesListener>(RolesListener);
@@ -14,5 +23,11 @@ describe("RolesListener", () => {
 
     it("should be defined", () => {
         expect(provider).toBeDefined();
+    });
+
+    it("should call the save method of the repository with the correct payload", async () => {
+        const payload = new Role();
+        await provider.save(payload);
+        expect(mockRepository.save).toHaveBeenCalledWith(payload);
     });
 });
