ab1170b6d61f13580fbd6fefa27314974c25fa06
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-06 09:11:56 +0300

    adding the database module to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { DatabaseModule } from "./core/databases/database.module";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, cache: true }),
+        DatabaseModule,
+    ],
 })
 export class AppModule {}
