0355b8ba8a40ef771b4aeb6e810d2a7850dd11a1
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-06 15:44:06 +0300

    downloading event emitter package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { EventEmitterModule } from "@nestjs/event-emitter";
 import { DatabaseModule } from "./core/databases/database.module";
 import { RolesModule } from "./modules/roles/roles.module";
 import { UsersModule } from "./modules/users/users.module";
@@ -7,6 +8,7 @@ import { UsersModule } from "./modules/users/users.module";
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true, cache: true }),
+        EventEmitterModule.forRoot({ global: true }),
         DatabaseModule,
         RolesModule,
         UsersModule,
