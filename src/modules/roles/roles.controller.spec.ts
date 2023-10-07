7a67aef9eed0f803133dfea5543b9a3f16caa4f8
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-07 09:35:46 +0300

    adding the role controller test for create

--- src/modules/roles/roles.controller.spec.ts
+++ src/modules/roles/roles.controller.spec.ts
@@ -1,20 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { RolesController } from './roles.controller';
-import { RolesService } from './roles.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { createRoleDto, rolesServiceMock } from "./constants/roles.mock";
+import { RolesController } from "./roles.controller";
+import { RolesService } from "./roles.service";
 
-describe('RolesController', () => {
-  let controller: RolesController;
+describe("RolesController", () => {
+    let controller: RolesController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [RolesController],
-      providers: [RolesService],
-    }).compile();
+    beforeAll(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [RolesController],
+            providers: [{ provide: RolesService, useValue: rolesServiceMock }],
+        }).compile();
 
-    controller = module.get<RolesController>(RolesController);
-  });
+        controller = module.get<RolesController>(RolesController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it("RolesController: should be defined", () => {
+        expect(controller).toBeDefined();
+    });
+
+    it("RolesController.create: should call create", async () => {
+        await controller.create(createRoleDto);
+        expect(rolesServiceMock.create).toBeCalledWith(createRoleDto);
+        expect(rolesServiceMock.create).toBeCalledTimes(1);
+    });
 });
