aa565ffb30abaed14a4b6aa03b4198e0ea0f7b11
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-07 09:33:08 +0300

    update the roles service naming

--- src/modules/roles/roles.service.spec.ts
+++ src/modules/roles/roles.service.spec.ts
@@ -1,7 +1,7 @@
 import { EventEmitter2 } from "@nestjs/event-emitter";
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
-import { roleRepositoryMock } from "./constants/roles.mock";
+import { roleRepositoryMock, rolesDbMock } from "./constants/roles.mock";
 import { CreateRoleDto } from "./dto/create-role.dto";
 import { Role } from "./entities/role.entity";
 import { RolesService } from "./roles.service";
@@ -27,39 +27,34 @@ describe("RolesService", () => {
         service = module.get<RolesService>(RolesService);
     });
 
-    it("should be defined", () => {
+    it("RolesService: should be defined", () => {
         expect(service).toBeDefined();
     });
 
-    it("role.findOne: should not return the mock data", async () => {
+    it("RolesService.findOne: should not return the mock data", async () => {
         const result = await service.findOne({
             where: { name: "khaled" },
         });
         expect(result).toBeUndefined();
     });
 
-    it("role.findOne: should return the mock data", async () => {
+    it("RolesService.findOne: should return the mock data", async () => {
         const result = await service.findOne({
             where: { name: "exist" },
         });
-        expect(result).toEqual({
-            role_id: "6d6d32d7-41f5-46ec-b772-41d8cf4a59ef",
-            name: "exist",
-            description: "some stuff",
-            users: [],
-        });
+        expect(result).toEqual(rolesDbMock[0]);
     });
 
-    it("role.isUnique: should not throw when unique", async () => {
+    it("RolesService.isUnique: should not throw when unique", async () => {
         const role = await service.isUnique("name");
         expect(role).toBeTruthy();
     });
 
-    it("role.isUnique: should throw when not unique", async () => {
+    it("RolesService.isUnique: should throw when not unique", async () => {
         await expect(() => service.isUnique("exist")).rejects.toThrow();
     });
 
-    it("role.create: should create", async () => {
+    it("RolesService.create: should create", async () => {
         const isUniqueSpy = jest.spyOn(service, "isUnique");
         const createRoleDto: CreateRoleDto = { name: "not " };
         const role = await service.create(createRoleDto);
