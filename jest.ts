fd16989ad396f595522d68f15ce47842b030bcdd
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-07 12:12:47 +0300

    using swc in testing instead of ts-jest

--- jest.ts
+++ jest.ts
@@ -7,7 +7,7 @@ module.exports = {
     rootDir: "src",
     testRegex: ".*\\.spec\\.ts$",
     transform: {
-        "^.+\\.(t|j)s$": "ts-jest",
+        "^.+\\.(t|j)s$": ["@swc/jest"],
     },
     collectCoverageFrom: ["**/*.(t|j)s"],
     coverageDirectory: "../coverage",
