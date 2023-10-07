a26baeef44e515bb67d8688aff443945567acc46
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-07 22:38:06 +0300

    adding the cryptograph service spec

--- src/core/utils/cryptograph/cryptograph.service.spec.ts
+++ src/core/utils/cryptograph/cryptograph.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { hashSync } from "bcrypt";
 import { CryptographService } from "./cryptograph.service";
 
 describe("CryptographService", () => {
@@ -11,7 +12,28 @@ describe("CryptographService", () => {
 
         service = module.get<CryptographService>(CryptographService);
     });
+
     it("should be defined", () => {
         expect(service).toBeDefined();
+    });
+
+    it("CryptographService.hash: it should hash", () => {
+        const data = "my name is password";
+        const hashData = service.hash(data);
+        expect(hashData).not.toEqual(data);
+    });
+
+    it("CryptographService.compare: it should return true when hash is valid", () => {
+        const data = "my name is password";
+        const hashData = hashSync(data, 10);
+        const result = service.compare(data, hashData);
+        expect(result).toBeTruthy();
+    });
+
+    it("CryptographService.compare: it should return false when hash is not valid", () => {
+        const data = "my name is password";
+        const hashData = hashSync(data, 10) + "fljf";
+        const result = service.compare(data, hashData);
+        expect(result).toBeFalsy();
     });
 });
