9177a58a222dea93d015b5b8b622c0fb7adc6ad0
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-07 09:35:24 +0300

    adding the roles service mock

--- src/modules/roles/constants/roles.mock.ts
+++ src/modules/roles/constants/roles.mock.ts
@@ -1,6 +1,11 @@
+import { CreateRoleDto } from "../dto/create-role.dto";
 import { Role } from "../entities/role.entity";
 
-const rolesDbMock: Role[] = [
+export const createRoleDto: CreateRoleDto = {
+    name: "stuff",
+};
+
+export const rolesDbMock: Role[] = [
     {
         role_id: "6d6d32d7-41f5-46ec-b772-41d8cf4a59ef",
         name: "exist",
@@ -15,4 +20,12 @@ export const roleRepositoryMock = {
     findOne: jest.fn((options) =>
         rolesDbMock.find((role) => role["name"] == options.where.name)
     ),
+};
+
+export const rolesServiceMock = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
 };
