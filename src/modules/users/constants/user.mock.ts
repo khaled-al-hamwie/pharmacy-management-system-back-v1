3ece28c4b2d83378ac76dbcf618a63ae7fd25489
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-08 15:20:38 +0300

    adding user mock

---
+++ src/modules/users/constants/user.mock.ts
@@ -0,0 +1,16 @@
+import { CreateUserDto } from "../dto/create-user.dto";
+
+export const userRepositoryMock = {
+    save: jest.fn(),
+    create: jest.fn((x) => x),
+    findOne: jest.fn(),
+    softRemove: jest.fn(),
+};
+
+export const createUserDto: CreateUserDto = {
+    role_id: "11223344",
+    first_name: "bla",
+    last_name: "bla",
+    email: "bla@gmail",
+    password: "blabla",
+};
