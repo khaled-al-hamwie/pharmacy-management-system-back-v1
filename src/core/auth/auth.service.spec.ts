47fceabb647c7f92b5abfa6de4f9d4ce7edfc7cc
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-08 17:52:09 +0300

    adding the auth service spec

--- src/core/auth/auth.service.spec.ts
+++ src/core/auth/auth.service.spec.ts
@@ -1,18 +1,34 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import {
+    createUserDto,
+    usersServiceMock,
+} from "../../modules/users/constants/user.mock";
+import { UsersService } from "../../modules/users/users.service";
+import { JJwtServiceMock } from "../constants/core.mock";
+import { JJwtService } from "../utils/jwt/j-jwt.service";
+import { AuthService } from "./auth.service";
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe("AuthService", () => {
+    let service: AuthService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                { provide: UsersService, useValue: usersServiceMock },
+                { provide: JJwtService, useValue: JJwtServiceMock },
+            ],
+        }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
+        service = module.get<AuthService>(AuthService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(service).toBeDefined();
+    });
+
+    it("AuthService.register: should create a user and sign token", () => {
+        service.register(createUserDto);
+        expect(usersServiceMock.create).toBeCalledWith(createUserDto);
+    });
 });
