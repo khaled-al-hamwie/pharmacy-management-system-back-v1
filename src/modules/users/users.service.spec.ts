6f9cb45c18e4436f694af7dfc82c808b2953680b
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-08 15:20:18 +0300

    adding create service

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,18 +1,51 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import {
+    CryptographServiceMock,
+    EmitterMock,
+} from "../../core/constants/core.mock";
+import { CryptographService } from "../../core/utils/cryptograph/cryptograph.service";
+import { rolesServiceMock } from "../roles/constants/roles.mock";
+import { RolesService } from "../roles/roles.service";
+import { UserSaveEvent } from "./constants/user.event";
+import { createUserDto, userRepositoryMock } from "./constants/user.mock";
+import { User } from "./entities/user.entity";
+import { UsersService } from "./users.service";
 
-describe('UsersService', () => {
-  let service: UsersService;
+describe("UsersService", () => {
+    let service: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    beforeAll(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                UsersService,
+                {
+                    provide: getRepositoryToken(User),
+                    useValue: userRepositoryMock,
+                },
+                { provide: RolesService, useValue: rolesServiceMock },
+                { provide: EventEmitter2, useValue: EmitterMock },
+                {
+                    provide: CryptographService,
+                    useValue: CryptographServiceMock,
+                },
+            ],
+        }).compile();
 
-    service = module.get<UsersService>(UsersService);
-  });
+        service = module.get<UsersService>(UsersService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(service).toBeDefined();
+    });
+
+    it("UsersService.create: should create", async () => {
+        const isUniqueSpy = jest.spyOn(service, "isUnique");
+        const user = await service.create(createUserDto);
+        expect(isUniqueSpy).toHaveBeenCalledWith(createUserDto.email);
+        expect(CryptographServiceMock.b_hash).toBeCalled();
+        expect(userRepositoryMock.create).toBeCalledWith(createUserDto);
+        expect(EmitterMock.emit).toBeCalledWith(UserSaveEvent, user);
+    });
 });
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable } from "@nestjs/common";
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOneOptions, Repository } from "typeorm";
+import { CryptographService } from "../../core/utils/cryptograph/cryptograph.service";
+import { RolesService } from "../roles/roles.service";
+import { UserSaveEvent } from "./constants/user.event";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { User } from "./entities/user.entity";
+import { userForbiddenException } from "./exceptions/user.forbidden.exception";
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+    constructor(
+        @InjectRepository(User) private readonly repository: Repository<User>,
+        private readonly rolesService: RolesService,
+        private readonly cryptographService: CryptographService,
+        private readonly eventEmitter: EventEmitter2
+    ) {}
+    async create(createUserDto: CreateUserDto) {
+        await this.rolesService.findById(createUserDto.role_id);
+        await this.isUnique(createUserDto.email);
+        const password = await this.cryptographService.b_hash(
+            createUserDto.password
+        );
+        createUserDto.password = password;
+        const user = this.repository.create(createUserDto);
+        this.eventEmitter.emit(UserSaveEvent, user);
+        return user;
+    }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    findAll() {
+        return `This action returns all users`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    findOne(options: FindOneOptions<User>) {
+        return this.repository.findOne(options);
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} user`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} user`;
+    }
+
+    async isUnique(email: string) {
+        const user = await this.findOne({ where: { email } });
+        if (user) throw new userForbiddenException(email);
+        return true;
+    }
 }
