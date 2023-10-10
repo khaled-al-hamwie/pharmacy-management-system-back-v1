fba3d4d6e25b8c6992ccec579584ca15dd006fc2
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-10 22:47:05 +0300

    adding the register to the path of auth route

--- src/core/auth/auth.controller.ts
+++ src/core/auth/auth.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { CreateUserDto } from "../../modules/users/dto/create-user.dto";
 import { AuthService } from "./auth.service";
+import { LogInGuard } from "./guards/log-in.guard";
 
 @Controller("auth")
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @Post()
+    @Post("register")
     register(@Body() createAuthDto: CreateUserDto) {
         return this.authService.register(createAuthDto);
+    }
+
+    @Get("login")
+    @UseGuards(LogInGuard)
+    login() {
+        return "test";
     }
 }
