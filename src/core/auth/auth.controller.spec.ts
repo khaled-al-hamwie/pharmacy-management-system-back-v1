f01668b678fb2975cca9ba380bc9844f3057f590
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-11 00:26:32 +0300

    adding auth controller test

--- src/core/auth/auth.controller.spec.ts
+++ src/core/auth/auth.controller.spec.ts
@@ -1,20 +1,36 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
-describe('AuthController', () => {
-  let controller: AuthController;
+const authServiceMock = { login: jest.fn(), register: jest.fn() };
+describe("AuthController", () => {
+    let controller: AuthController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [AuthController],
+            providers: [
+                {
+                    provide: AuthService,
+                    useValue: authServiceMock,
+                },
+            ],
+        }).compile();
 
-    controller = module.get<AuthController>(AuthController);
-  });
+        controller = module.get<AuthController>(AuthController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it("AuthController: should be defined", () => {
+        expect(controller).toBeDefined();
+    });
+
+    it("AuthController.register: should call register service", () => {
+        controller.register({ email: "bla" } as any);
+        expect(authServiceMock.register).toBeCalledWith({ email: "bla" });
+    });
+
+    it("AuthController.login: should call login service", () => {
+        controller.login({ email: "bla" } as any);
+        expect(authServiceMock.login).toBeCalledWith({ email: "bla" });
+    });
 });
