a1375b7035f0a3d96693c3ec64244e7a260efa88
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-10 23:42:21 +0300

    adding auth strategy test for check email

--- src/core/auth/auth.service.spec.ts
+++ src/core/auth/auth.service.spec.ts
@@ -4,9 +4,14 @@ import {
     usersServiceMock,
 } from "../../modules/users/constants/user.mock";
 import { UsersService } from "../../modules/users/users.service";
-import { JJwtServiceMock } from "../constants/core.mock";
+import {
+    CryptographServiceMock,
+    JJwtServiceMock,
+} from "../constants/core.mock";
+import { CryptographService } from "../utils/cryptograph/cryptograph.service";
 import { JJwtService } from "../utils/jwt/j-jwt.service";
 import { AuthService } from "./auth.service";
+import { CredentailsDontMatchException } from "./exceptions/credentails-dont-match.exception";
 
 describe("AuthService", () => {
     let service: AuthService;
@@ -17,6 +22,10 @@ describe("AuthService", () => {
                 AuthService,
                 { provide: UsersService, useValue: usersServiceMock },
                 { provide: JJwtService, useValue: JJwtServiceMock },
+                {
+                    provide: CryptographService,
+                    useValue: CryptographServiceMock,
+                },
             ],
         }).compile();
 
@@ -30,5 +39,16 @@ describe("AuthService", () => {
     it("AuthService.register: should create a user and sign token", () => {
         service.register(createUserDto);
         expect(usersServiceMock.create).toBeCalledWith(createUserDto);
+    });
+
+    it("AuthService.checkEmail: should return user when it find one", async () => {
+        const user = await service.checkEmail("found@gmail.com");
+        expect(user.password).toBe("bla");
+    });
+
+    it("AuthService.checkEmail: should throw error when user not found", async () => {
+        await expect(() =>
+            service.checkEmail("no@gmail.com")
+        ).rejects.toThrowError(CredentailsDontMatchException);
     });
 });
